602644d9a69dbeeb71164f592faf1fb48d1daf6d
Howard <a0981456759@gmail.com>
2021-01-25 03:37:37 +0000

    bfsqlite

--- database/seeders/CollectionSeeder.php
+++ database/seeders/CollectionSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Collection;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\DB;  
 
 class CollectionSeeder extends Seeder
 {
